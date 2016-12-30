1f23c4e87e39d0640e9ded9b2acbbecfbb197200
Tom M <tmielcza@student.42.fr>
2016-12-30 22:16:08 +0100

    rename type `Tokens` to `Token` (consistency)

--- src/Parse.hs
+++ src/Parse.hs
@@ -6,7 +6,7 @@ import Debug.Trace
 import Data.Char
 import Test.HUnit
 
-data Tokens = Letter Char | Operator Ope | Bang
+data Token = Letter Char | Operator Ope | Bang
     deriving(Show)
 data Ope = Xor | Or | And 
     deriving(Eq, Ord)
@@ -23,7 +23,7 @@ instance Show Expr where
     show (Fact c) = [c]
     show (Not expr) = "!" ++ (show expr)
 
-charToToken :: Char -> Either String Tokens
+charToToken :: Char -> Either String Token
 charToToken c
     | c == '|' = Right (Operator Or)
     | c == '+'  = Right (Operator And)
@@ -35,35 +35,35 @@ charToToken c
 add_expr op Nothing r = Just r
 add_expr op (Just l) r = Just (Grp op l r)
 
-ast_xor :: Maybe Expr -> [Tokens] -> (Maybe Expr, [Tokens])
+ast_xor :: Maybe Expr -> [Token] -> (Maybe Expr, [Token])
 ast_xor expr rest =
   case ast_or Nothing rest of
     (Just expr_d, (Operator Xor):rest_d) -> ast_xor (add_expr Xor expr expr_d) rest_d
     (Just expr_d, rest_d) -> ((add_expr Xor expr expr_d), rest_d)
     other -> other
 
-ast_or :: Maybe Expr -> [Tokens] -> (Maybe Expr, [Tokens])
+ast_or :: Maybe Expr -> [Token] -> (Maybe Expr, [Token])
 ast_or expr rest =
   case ast_and Nothing rest of
     (Just expr_d, (Operator Or):rest_d) -> ast_or (add_expr Or expr expr_d) rest_d
     (Just expr_d, rest_d) -> ((add_expr Or expr expr_d), rest_d)
     other -> other
 
-ast_and :: Maybe Expr -> [Tokens] -> (Maybe Expr, [Tokens])
+ast_and :: Maybe Expr -> [Token] -> (Maybe Expr, [Token])
 ast_and expr rest =
   case ast_not rest of
     (Just expr_d, (Operator And):rest_d) -> ast_and (add_expr And expr expr_d) rest_d
     (Just expr_d, rest_d) -> ((add_expr And expr expr_d), rest_d)
     other -> other
 
-ast_not :: [Tokens] -> (Maybe Expr, [Tokens])
+ast_not :: [Token] -> (Maybe Expr, [Token])
 ast_not (Bang:rest) =
   case ast_not rest of
     (Just expr_d, rest_d) -> (Just (Not expr_d), rest_d)
     other -> other
 ast_not rest = ast_fact rest
 
-ast_fact :: [Tokens] -> (Maybe Expr, [Tokens])
+ast_fact :: [Token] -> (Maybe Expr, [Token])
 ast_fact (Letter f:rest) = (Just (Fact f), rest)
 ast_fact rest = (Nothing, rest)
 
