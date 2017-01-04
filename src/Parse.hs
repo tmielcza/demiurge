aca19efc86dceb3ea5c0946af24501fb0d47d259
Celine DANNAPPE <celine.dannappe@gmail.com>
2017-01-04 01:29:44 +0100

    adapt lexingto multi char tokens

--- src/Parse.hs
+++ src/Parse.hs
@@ -4,34 +4,30 @@ module Parse
 
 import Debug.Trace
 import Data.Char
+import Control.Monad
 
 data Token = Letter Char | Operator Ope | Bang | LParen | RParen
     deriving(Show)
-data Ope = Xor | Or | And
+data Ope = Xor | Or | And | Eq | Imply
     deriving(Eq, Ord)
 data Expr = Grp Ope Expr Expr | Fact Char | Not Expr
     deriving(Eq)
+
 
 instance Show Ope where
     show Or = "|"
     show And = "+"
     show Xor = "^"
+    show Eq = "<=>"
+    show Imply = "=>"
+
 
 instance Show Expr where
     show (Grp o e1 e2) = "(" ++ show e1 ++ show o ++ show e2 ++ ")"
     show (Fact c) = [c]
     show (Not expr) = "!" ++ show expr
 
-charToToken :: Char -> Either String Token
-charToToken c
-    | c == '|' = Right (Operator Or)
-    | c == '+'  = Right (Operator And)
-    | c == '^' = Right (Operator Xor)
-    | c == '!' = Right Bang
-    | c == '(' = Right LParen
-    | c == ')' = Right RParen
-    | isAlpha c = Right (Letter c)
-    | otherwise = Left $ "Lexing error near: " ++ show c
+
 
 addExpr op Nothing r = Just r
 addExpr op (Just l) r = Just (Grp op l r)
@@ -87,7 +83,40 @@ ast tokens =
     _ -> Left "Empty expression"
 
 tokenize :: String -> Either String [Token]
-tokenize = mapM charToToken . filter (/= ' ')
+--tokenize = mapM charToToken . filter (/= ' ')
+tokenize str = fmap (reverse . snd)  (foldM toToken  ("", []) ( filter (/= ' ') str))
+
+charToToken :: Char -> Either String Token
+charToToken c
+    | c == '|' = Right (Operator Or)
+    | c == '+'  = Right (Operator And)
+    | c == '^' = Right (Operator Xor)
+    | c == '!' = Right Bang
+    | c == '(' = Right LParen
+    | c == ')' = Right RParen
+    | isAlpha c = Right (Letter c)
+    | otherwise = Left $ "Lexing error near: " ++ show c
+
+toToken:: (String, [Token]) -> Char -> Either String (String, [Token])
+
+
+toToken ("" , acc) c
+  | c == '+' = Right ("", Operator And:acc)
+  | c == '|' = Right ("", Operator Or:acc)
+  | c == '^' = Right ("", Operator Xor:acc)
+  | c == '!' = Right ("", Bang:acc)
+  | c == '(' = Right ("", LParen:acc)
+  | c == ')' = Right ("", RParen:acc)
+  | c == '<' = Right ("<", acc)
+  | c == '=' = Right ("=", acc)
+  | isAlpha c = Right ("", Letter c:acc)
+  | otherwise = Left ("Lexical error near" ++ [c])
+--toToken ("" , acc) '<' = Right ("<", acc)
+toToken ("<" , acc) '=' = Right ("<=", acc)
+toToken ("<=" , acc) '>' = Right ("", Operator Eq:acc)
+--toToken ("" , acc) '=' = Right ("=", acc)
+toToken ("=" , acc) '>' = Right ("", Operator Imply:acc)
+toToken _ c = Left ("Lexical error near" ++ [c])
 
 parse :: String -> Either String Expr
 parse str = tokenize str >>= ast
