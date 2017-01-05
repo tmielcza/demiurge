35d090c63b31bf0549049e50a63ee80349433ba8
Celine DANNAPPE <celine.dannappe@gmail.com>
2017-01-05 16:43:01 +0100

    test relational operator

--- test/Parse/Test.hs
+++ test/Parse/Test.hs
@@ -7,41 +7,55 @@ import Parse
 import Data.Either.Unwrap
 
 parseTest str expect = (testCase str . assertEqual "" expect . show . fromRight . parse) str
-
 parseErrorTest str = (testCase str . assertBool str . isLeft . parse) str
+
+astXorTest str expect = (testCase str . assertEqual "" expect . show . fromRight .  (>>= (checkReturn . (astXor Nothing))) . tokenize) str
+astXorErrorTest str = (testCase str . assertBool str . isLeft . (>>= (checkReturn . (astXor Nothing))) . tokenize) str
+
 
 parseSuite = testGroup "Parsing tests"
              [
-               -- fact parsing tests
-               parseTest "     A        " "A",
-               parseErrorTest "&",
-               parseErrorTest "1",
-               parseErrorTest "",
-               -- op parsing tests
-               parseTest "A+B+C" "((A+B)+C)",
-               parseTest "A^B|C+D" "(A^(B|(C+D)))",
-               parseTest "A^B|C|D" "(A^((B|C)|D))",
-               parseTest "A^B|C^D" "((A^(B|C))^D)",
-               parseTest "A^B|C+D^E" "((A^(B|(C+D)))^E)",
-               parseErrorTest "+",
-               parseErrorTest "AB",
-               parseErrorTest "A+|",
-               parseErrorTest "A|B+C^",
-               parseErrorTest "A|A||",
-               -- not parsing tests
-               parseTest "!A" "!A",
-               parseTest "!A+!B" "(!A+!B)",
-               parseTest "!!!!!!A^!!!!!B" "(!!!!!!A^!!!!!B)",
-               parseErrorTest "!",
-               parseErrorTest "!!!!",
-               -- parentheses parsing tests
-               parseTest "c | (d + y)" "(c|(d+y))" ,
-               parseTest "c ^ !(r | i) + p" "(c^(!(r|i)+p))" ,
-               parseTest "c + !(r | i) ^ p" "((c+!(r|i))^p)" ,
-               parseTest "c + !(r | (v ^ o)) ^ p" "((c+!(r|(v^o)))^p)" ,
-               parseTest "(U)" "U",
-               parseErrorTest "(a + b",
-               parseErrorTest "(a + b +)",
-               parseErrorTest "(a + b ))",
-               parseErrorTest "()"
+               -- fact tests
+               astXorTest "     A        " "A",
+               astXorErrorTest "&",
+               astXorErrorTest "1",
+               astXorErrorTest "",
+               -- op tests
+               astXorTest "A+B+C" "((A+B)+C)",
+               astXorTest "A^B|C+D" "(A^(B|(C+D)))",
+               astXorTest "A^B|C|D" "(A^((B|C)|D))",
+               astXorTest "A^B|C^D" "((A^(B|C))^D)",
+               astXorTest "A^B|C+D^E" "((A^(B|(C+D)))^E)",
+               astXorErrorTest "+",
+               astXorErrorTest "AB",
+               astXorErrorTest "A+|",
+               astXorErrorTest "A|B+C^",
+               astXorErrorTest "A|A||",
+               -- not tests
+               astXorTest "!A" "!A",
+               astXorTest "!A+!B" "(!A+!B)",
+               astXorTest "!!!!!!A^!!!!!B" "(!!!!!!A^!!!!!B)",
+               astXorErrorTest "!",
+               astXorErrorTest "!!!!",
+               -- parentheses tests
+               astXorTest "C | (D + Y)" "(C|(D+Y))" ,
+               astXorTest "C ^ !(R | I) + P" "(C^(!(R|I)+P))" ,
+               astXorTest "C + !(R | I) ^ P" "((C+!(R|I))^P)" ,
+               astXorTest "C + !(R | (V ^ O)) ^ P" "((C+!(R|(V^O)))^P)" ,
+               astXorTest "(U)" "U",
+               astXorErrorTest "(A + B",
+               astXorErrorTest "(A + B +)",
+               astXorErrorTest "(A + B ))",
+               astXorErrorTest "()",
+               --Implication tests
+               parseTest "A => B" "(A=>B)",
+               parseTest "A <=> B" "(A<=>B)",
+               parseTest "A + C <=> B" "((A+C)<=>B)",
+               parseTest "A ^ C <=> B | D" "((A^C)<=>(B|D))",
+               parseTest "A + (C | P) <=> B" "((A+(C|P))<=>B)",
+               parseErrorTest "A => + B",
+               parseErrorTest "A + => B"
+               parseErrorTest "A => B => C"
+               parseErrorTest "(B => C)"
+
              ]
