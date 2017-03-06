952b2376bb35528cef637bb9cfd8fb17a7ceec33
cdannapp <celine.dannappe@gmail.com>
2017-03-06 11:54:05 +0100

    fix inference file

--- src/Inference.hs
+++ src/Inference.hs
@@ -7,29 +7,29 @@ import Types
 
 infer :: [Relation] -> Expr -> [([Relation], Relation)]
 infer stack@((premices `Imply` (lhs `And` rhs)):_) goal =
-  infer (premices `Imply` rhs):stack goal ++
-  infer (premices `Imply` lhs):stack goal
+  infer ((premices `Imply` rhs):stack) goal ++
+  infer ((premices `Imply` lhs):stack) goal
 
 infer stack@((premices `Imply` (lhs `Or` rhs)):_) goal =
-  infer ((premices `And` Not lhs) `Imply` rhs):stack goal ++
-  infer ((premices `And` Not rhs) `Imply` lhs):stack goal
+  infer (((premices `And` Not lhs) `Imply` rhs):stack) goal ++
+  infer (((premices `And` Not rhs) `Imply` lhs):stack) goal
 
 infer stack@((premices `Imply` (lhs `Xor` rhs)):_) goal =
-  infer (premices `Imply` ((lhs `Or` rhs) `And` Not (lhs `And` rhs))):stack goal
+  infer ((premices `Imply` ((lhs `Or` rhs) `And` Not (lhs `And` rhs))):stack) goal
 
 
 -- inference of not rules
 infer stack@((rhs `Imply` Not (Not lhs)):_) goal =
-  infer (rhs `Imply` lhs):stack goal
+  infer ((rhs `Imply` lhs):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `And` rhs)):_) goal =
-  infer (premices `Imply` (Not lhs `Or` Not rhs)):stack goal
+  infer ((premices `Imply` (Not lhs `Or` Not rhs)):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `Or` rhs)):_) goal =
-  infer (premices `Imply` (Not lhs `And` Not rhs)):stack goal
+  infer ((premices `Imply` (Not lhs `And` Not rhs)):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `Xor` rhs)):_) goal =
-  infer (premices `Imply` (Not (lhs `Or` rhs) `Or` (lhs `And` rhs))):stack goal
+  infer ((premices `Imply` (Not (lhs `Or` rhs) `Or` (lhs `And` rhs))):stack) goal
 
 -- return the rule sent if the rhs is the fact we are looking for
 infer (r@(_ `Imply` fact):tail) goal
@@ -40,7 +40,7 @@ infer (r@(_ `Imply` fact):tail) goal
 
 --modus tollens or transposition
 launchInferences ( lhs `Imply` rhs) goal =
-  infer ( lhs `Imply` rhs) goal ++ infer ( Not rhs `Imply` Not lhs) goal
+  infer [( lhs `Imply` rhs)] goal ++ infer [( Not rhs `Imply` Not lhs)] goal
 
 -- distribution of Equivalence in implications
 launchInferences (rhs `Eq` lhs) goal =
