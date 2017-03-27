1cf41010457923cdafa157085674a6c7cef493ed
Tom M <tmielcza@student.42.fr>
2017-03-27 19:03:17 +0200

    make goals considered unsolved during proofs display

--- src/ReasoningVisualisation.hs
+++ src/ReasoningVisualisation.hs
@@ -52,7 +52,7 @@ showResolvedExpr (Xor e1 e2) = resolvedToString e1 e2 "^"
 showResolvedExpr (Or e1 e2) = resolvedToString e1 e2 "|"
 showResolvedExpr (And e1 e2) = resolvedToString e1 e2 "+"
 showResolvedExpr (Not e) = "!" ++* showResolvedExpr e
-showResolvedExpr (Fact fact) = getExistantInKnowledge fact (\(st, _) -> (return . show) st)
+showResolvedExpr (Fact fact) = getExistantInKnowledge fact (\(_, Known st) -> (return . show) st)
 
 getFacts :: Expr -> [Expr]
 getFacts (Xor e1 e2) = getFacts e1 ++ getFacts e2
@@ -71,8 +71,8 @@ rulesReasoning :: [Relation] -> KnowledgeState String
 rulesReasoning list@(rule@(lft `Imply` _):_)=
   let
     rulesInference = if (length list > 1) then showRulesTransformation list else ""
-    showSubgoal f (st, Known b) = return ("Fact "++ f ++ " has been initialised at " ++ (show st) ++ "\n")
-    showSubgoal f (st, p) = ("Fact " ++ f ++ " is " ++ (show st) ++ ":\n") ++* (showProof (Fact f) p)
+    showSubgoal f (_st, Known b) = return ("Fact "++ f ++ " is known as " ++ (show b) ++ "\n")
+    showSubgoal f (st, p) = ("Fact " ++ f ++ " is " ++ (show st) ++ ":\n") ++* (consumeProof (Fact f) p)
     reasoning = foldl (\prev (Fact x) -> (getExistantInKnowledge x (showSubgoal x)) *++* prev) (return "") (getFacts lft)
     conclusion = "so the goal is equal to " ++* (showResolvedExpr lft) *++ "\n"
   in rulesInference ++* reasoning *++* conclusion
@@ -84,38 +84,43 @@ setAsKnown:: Expr -> KnowledgeState String
 setAsKnown (Fact f) =
   getExistantInKnowledge f (\(state, _proof) -> do { S.modify (M.insert f (state, Known state)); return f})
 
+setAsUnknown:: Expr -> KnowledgeState String
+setAsUnknown (Fact f) =
+  getExistantInKnowledge f (\(state, _proof) -> do { S.modify (M.insert f (state, Known (Unsolved (Fact f)))); return f})
 
 showProof :: Expr -> Proof -> KnowledgeState String
-showProof goal (Invalid list1@(rule1:_) list2@(rule2:_)) = do
-  setAsKnown goal;
+showProof goal (Invalid list1@(rule1:_) list2@(rule2:_)) =
   (("There are different results for " ++ show goal ++ ":\n" ++ show rule1 ++ "\n") ++* rulesReasoning list1 *++ (show rule2 ++ "\n")) *++*
    rulesReasoning list2 *++ (show rule1 ++ " has a different result from " ++ show rule2 ++ "\n")
 
-showProof goal (Contradiction list@(rule:_) []) = do
-  setAsKnown goal;
+showProof goal (Contradiction list@(rule:_) []) =
   ("Their is a contradiction in the rule " ++ show rule) ++* rulesReasoning list
 
-showProof goal (Tautology list@(rule:_) []) = do
-  setAsKnown goal;
+showProof goal (Tautology list@(rule:_) []) =
   ("Their is a tautology in the rule " ++ show rule) ++* rulesReasoning list
 
 showProof goal (RuleProof list@(rule:_)) = do
-  setAsKnown goal;
+  k <- S.get
   ("The rule used for " ++ show goal ++ " is: " ++
      show rule ++ "\n") ++* rulesReasoning list *++ "\n"
 
-showProof goal (RuleProof []) = do
-  setAsKnown goal;
+showProof goal (RuleProof []) =
   return $ "No rule matches the goal " ++ show goal ++ "\n"
 
-showProof goal (Known st) = do
-  setAsKnown goal;
+showProof goal (Known st) =
   return $ "The fact " ++ show goal ++ " is known as " ++ show st ++ "\n"
 
 --showProof goal kn = return ("$$$$" ++ show kn)
 
+consumeProof :: Expr -> Proof -> KnowledgeState String
+consumeProof goal proof = do
+  setAsUnknown goal
+  s <- showProof goal proof
+  setAsKnown goal
+  return s
+
 runShowProof :: Knowledge -> Expr -> Proof -> String
-runShowProof k g p = fst $ S.runState (showProof g p) k
+runShowProof k g p = fst $ S.runState (consumeProof g p) k
 
 showFactResolution :: Knowledge -> Expr -> (String, Knowledge)
 showFactResolution k (Fact goal) =
