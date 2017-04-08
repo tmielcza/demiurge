98a3c83d5ef6770420e8c44d0062d706bb2dbf13
Celine DANNAPPE <celine.dannappe@gmail.com>
2017-04-08 18:05:55 +0200

    develop the opposite of an expresion when moved to left

--- src/Inference.hs
+++ src/Inference.hs
@@ -15,11 +15,11 @@ infer stack@(((lhs `Or` rhs) `Imply` conclusion):_) goal =
   infer ((rhs `Imply` conclusion):stack) goal
 
 infer stack@((premices `Imply` (lhs `Or` rhs)):_) goal =
-  infer (((premices `And` Not lhs) `Imply` rhs):stack) goal ++
-  infer (((premices `And` Not rhs) `Imply` lhs):stack) goal
+  infer (((premices `And` opposite lhs) `Imply` rhs):stack) goal ++
+  infer (((premices `And` opposite rhs) `Imply` lhs):stack) goal
 
 infer stack@((premices `Imply` (lhs `Xor` rhs)):_) goal =
-  infer ((premices `Imply` ((lhs `Or` rhs) `And` Not (lhs `And` rhs))):stack) goal
+  infer ((premices `Imply` ((lhs `Or` rhs) `And` ((Not lhs) `Or` (Not rhs)))):stack) goal
 
 
 -- inference of not rules
@@ -27,13 +27,13 @@ infer stack@((rhs `Imply` Not (Not lhs)):_) goal =
   infer ((rhs `Imply` lhs):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `And` rhs)):_) goal =
-  infer ((premices `Imply` (Not lhs `Or` Not rhs)):stack) goal
+  infer ((premices `Imply` (opposite (lhs `And` rhs))):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `Or` rhs)):_) goal =
-  infer ((premices `Imply` (Not lhs `And` Not rhs)):stack) goal
+  infer ((premices `Imply` (opposite (lhs `Or` rhs))):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `Xor` rhs)):_) goal =
-  infer ((premices `Imply` (Not (lhs `Or` rhs) `Or` (lhs `And` rhs))):stack) goal
+  infer ((premices `Imply` (opposite (lhs `Xor` rhs))):stack) goal
 
 -- return the rule sent if the rhs is the fact we are looking for
 infer list@(r@(_ `Imply` fact):_) goal
@@ -55,3 +55,10 @@ launchInferences eq@[(rhs `Eq` lhs)] goal =
 
 inferRules :: [Relation] -> Expr -> [([Relation], Relation)]
 inferRules rules goal = foldr (\r -> (++) ((launchInferences [r] goal))) [] rules
+
+opposite :: Expr -> Expr
+opposite (Fact f) = Not (Fact f)
+opposite (Not e) = e
+opposite (lhs `And` rhs) = (opposite lhs) `Or` (opposite rhs)
+opposite (lhs `Or` rhs) = (opposite lhs) `And` (opposite rhs)
+opposite (lhs `Xor` rhs) = ((opposite lhs) `And` (opposite rhs)) `Or` (lhs `And` rhs)
