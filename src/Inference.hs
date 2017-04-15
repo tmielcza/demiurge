382ed115b08cdc3a42a15e4e2b321ec731596d70
Celine DANNAPPE <celine.dannappe@gmail.com>
2017-04-15 15:14:16 +0200

    change function for DNF and CNF transformations

--- src/Inference.hs
+++ src/Inference.hs
@@ -4,6 +4,7 @@ module Inference
   ) where
 
 import Types
+import Debug.Trace
 
 infer :: [Relation] -> Expr -> [([Relation], Relation)]
 infer stack@((premices `Imply` (lhs `And` rhs)):_) goal =
@@ -15,25 +16,25 @@ infer stack@(((lhs `Or` rhs) `Imply` conclusion):_) goal =
   infer ((rhs `Imply` conclusion):stack) goal
 
 infer stack@((premices `Imply` (lhs `Or` rhs)):_) goal =
-  infer (((premices `And` opposite lhs) `Imply` rhs):stack) goal ++
-  infer (((premices `And` opposite rhs) `Imply` lhs):stack) goal
+  infer (((toDnf (premices `And` (Not lhs))) `Imply` rhs):stack) goal ++
+  infer (((toDnf (premices `And` (Not rhs))) `Imply` lhs):stack) goal
 
-infer stack@((premices `Imply` (lhs `Xor` rhs)):_) goal =
+{-infer stack@((premices `Imply` (lhs `Xor` rhs)):_) goal =
   infer ((premices `Imply` ((lhs `Or` rhs) `And` ((Not lhs) `Or` (Not rhs)))):stack) goal
-
+-}
 
 -- inference of not rules
 infer stack@((rhs `Imply` Not (Not lhs)):_) goal =
   infer ((rhs `Imply` lhs):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `And` rhs)):_) goal =
-  infer ((premices `Imply` (opposite (lhs `And` rhs))):stack) goal
+  infer ((premices `Imply` ((toCnf . Not) (lhs `And` rhs))):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `Or` rhs)):_) goal =
-  infer ((premices `Imply` (opposite (lhs `Or` rhs))):stack) goal
+  infer ((premices `Imply` ((toCnf . Not) (lhs `Or` rhs))):stack) goal
 
 infer stack@((premices `Imply` Not (lhs `Xor` rhs)):_) goal =
-  infer ((premices `Imply` (opposite (lhs `Xor` rhs))):stack) goal
+  infer ((premices `Imply` ((toCnf . Not) (lhs `Xor` rhs))):stack) goal
 
 -- return the rule sent if the rhs is the fact we are looking for
 infer list@(r@(_ `Imply` fact):_) goal
@@ -46,9 +47,9 @@ launchInferences :: [Relation] -> Expr -> [([Relation], Relation)]
 --modus tollens or transposition
 -- here the xs is the possible equivalence (or an empty list)
 launchInferences list@((lhs `Imply` rhs):xs) goal
-  | rhs == Not lhs = infer ((toDNF lhs `Imply` toCNF rhs):(lhs `Imply` rhs):xs) goal
-  | otherwise = infer ((toDNF lhs `Imply` toCNF rhs):(lhs `Imply` rhs):xs) goal
-    ++ infer ((toDNF (Not rhs) `Imply` toCNF (Not lhs)):(Not rhs `Imply` Not lhs):list) goal
+  | rhs == Not lhs =  infer ((toDnf lhs `Imply` toCnf rhs):(lhs `Imply` rhs):xs) goal
+  | otherwise = infer ((toDnf lhs `Imply` toCnf rhs):(lhs `Imply` rhs):xs) goal
+    ++ infer ((toDnf (Not rhs) `Imply` toCnf (Not lhs)):(Not rhs `Imply` Not lhs):list) goal
 
 -- distribution of Equivalence in implications
 launchInferences eq@[(rhs `Eq` lhs)] goal =
@@ -65,26 +66,29 @@ opposite (lhs `And` rhs) = (opposite lhs) `Or` (opposite rhs)
 opposite (lhs `Or` rhs) = (opposite lhs) `And` (opposite rhs)
 -- opposite e = Not e
 
-xorDNF (lhs `Xor` rhs) = toDNF ((opposite . toCNF) lhs `And` toDNF rhs) `Or` (toDNF lhs `And` (opposite . toCNF) rhs)
-xorDNF (lhs `And` rhs) = toDNF lhs `And` toDNF rhs
-xorDNF (lhs `Or` rhs) = toDNF lhs `Or` toDNF rhs
-xorDNF fact@(Fact _) = fact
-xorDNF fact@(Not (Fact _)) = fact
-xorDNF (Not e) = (opposite . toCNF) e
+transformCnf (a `Or` (b `And` c)) =
+  let abr = toCnf a in ((abr `Or` toCnf b) `And` (abr `Or` toCnf c))
+transformCnf (a `Xor` b) = ((toCnf a) `Or` (toCnf (Not b))) `And` (((toCnf (Not a)) `Or` (toCnf b)))
+transformCnf not@(Not (Fact _)) = not
+transformCnf not@(Not e) = (toCnf . opposite) (toDnf e)
+transformCnf e = e
 
-xorCNF (lhs `Xor` rhs) = toCNF ((opposite . toDNF) lhs `Or` (opposite . toDNF) rhs) `And` (toCNF lhs `Or` toCNF rhs)
-xorCNF (lhs `And` rhs) = toCNF lhs `And` toCNF rhs
-xorCNF (lhs `Or` rhs) = toCNF lhs `Or` toCNF rhs
-xorCNF fact@(Fact _) = fact
-xorCNF fact@(Not (Fact _)) = fact
-xorCNF (Not e) = (opposite . toDNF) e
+transformDnf (a `And` (b `Or` c)) =
+  let abr = toDnf a in ((abr `And` toDnf b) `Or` (abr `And` toDnf c))
+transformDnf (a `Xor` b) = (toDnf a) `And` (toDnf (Not b)) `Or` ((toDnf (Not a)) `And` (toDnf b))
+transformDnf not@(Not (Fact _)) = not
+transformDnf not@(Not e) = (toDnf . opposite) (toCnf e)
+transformDnf e = e
 
-toCNF (a `Or` (b `And` c)) = toCNF (xorCNF a `Or` xorCNF b) `And` toCNF (xorCNF a `Or` xorCNF c)
-toCNF ((a `And` b) `Or` c) = toCNF (xorCNF c `Or` xorCNF a) `And` toCNF (xorCNF c `Or` xorCNF b)
-toCNF fact@(Fact _) = fact
-toCNF e = xorCNF e
+browse transform (a `And` b) = transform (browse transform a `And` browse transform b)
+browse transform (a `Or` b) = transform (browse transform a `Or` browse transform b)
+browse transform (a `Xor` b) = transform (browse transform a `Xor` browse transform b)
+browse transform e = transform e
 
-toDNF (a `And` (b `Or` c)) = toCNF (xorDNF a `And` xorDNF b) `Or` toCNF (xorDNF a `And` xorDNF c)
-toDNF ((a `Or` b) `And` c) = toCNF (xorDNF c `And` xorDNF a) `Or` toCNF (xorDNF c `And` xorDNF b)
-toDNF fact@(Fact _) = fact
-toDNF e = xorDNF e
+toCnf :: Expr -> Expr
+toCnf = browse transformCnf
+toDnf :: Expr -> Expr
+toDnf = browse transformDnf
+
+
+
